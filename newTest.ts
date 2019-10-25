c05d17b66c4eb95a3c28e6fe41ae6897791825ab
Darshan <darshanmarathe@gmail.com>
2019-10-25 17:03:29 +0530

    read and write added

--- newTest.ts
+++ newTest.ts
@@ -1,6 +1,16 @@
 import { SELECT, FROM, WHERE, GROUPBY } from "./node_sql";
+const fs = require("fs")
 
-
-var values =  SELECT(["versionnumber version" , "rocket_envelopename fullname" , "createdon createdAt"] , FROM('./19-10-2019.json'));
+var values =  SELECT(["versionnumber version" , "rocket_envelopename fullname" , "createdon createdAt"] , 
+FROM('./19-10-2019.json'));
 
 console.log(values)
+
+fs.writeFile("19-10-2019.out.json", values, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+ 
+    console.log("JSON file has been saved.");
+});
